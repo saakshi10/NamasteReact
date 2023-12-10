ff7ea3d00090a6f4d590c12a87d9f70eb7aa7529
unknown <saakshi.singhal10@gmail.com>
2023-12-10 12:04:22 +0530

    restaurant menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { IMG_CDN_URL } from "../config";
+import Shimmer from "./Shimmer";
 
 const RestaurantMenu = (props) => {
     const params = useParams();
     const { id } = params;
 
     const [restaurantDetails, setRestaurantDetails] = useState({});
+    const [restaurantMenu, setRestaurantMenu] = useState([]);
 
     useEffect(() => {
         getRestaurantInfo();
@@ -21,14 +23,41 @@ const RestaurantMenu = (props) => {
         const json = await data.json();
 
         setRestaurantDetails(json?.data?.cards[0]?.card?.card?.info);
-
-        // const len = json?.data?.cards?.length;
-        // if(len){
-        //     setRestaurantMenu(json?.data?.cards[0]?.cards(len-1)?.groupedCard?.cardGroupMap.REGULAR?.cards
-        // }
-
-        console.log(json);
+        getRestaurantMenu(json);
     }
+
+    const getRestaurantMenu = (restaurantData) => {
+        let menuCard;
+
+        restaurantData?.data?.cards?.forEach((card) => {
+            const cardKeys = Object.keys(card);
+
+            let hasMenu = cardKeys.find((item) => {
+                return item === "groupedCard" ? true : false;
+            });
+            if (hasMenu) {
+                console.log(hasMenu);
+                menuCard = card;
+                return;
+            }
+        });
+
+        getMenuItems(menuCard?.groupedCard?.cardGroupMap?.REGULAR?.cards);
+    };
+
+    const getMenuItems = (menuCardsList) => {
+        const menuId =
+            "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory";
+        const menuList = [];
+        console.log(menuCardsList);
+        menuCardsList.forEach((card) => {
+            if (card?.card?.card["@type"] === menuId) {
+                menuList.push(card?.card?.card);
+            }
+        });
+        console.log(menuList);
+        setRestaurantMenu(menuList);
+    };
 
     return (
         <div>
@@ -41,6 +70,32 @@ const RestaurantMenu = (props) => {
 
             <div className="menu">
                 <h3>Menu: </h3>
+                {restaurantMenu?.length <= 0 ? (
+                    <Shimmer />
+                ) : (
+                    // <div>test data</div>
+                    restaurantMenu.map((menuItem) => {
+                        return (
+                            <ul
+                                className="category-title"
+                                key={"menu-" + menuItem.title}
+                            >
+                                {menuItem.title}
+                                {/* display sub items */}
+                                {menuItem?.itemCards?.map((item) => {
+                                    return (
+                                        <li
+                                            key={"item-" + item.card.info.id}
+                                            className="item-title"
+                                        >
+                                            {item?.card?.info?.name}
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        );
+                    })
+                )}
                 {/* {console.log(restaurantDetails?)} */}
             </div>
         </div>
